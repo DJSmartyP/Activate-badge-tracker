6f4056e27546a349df3ff973d646d422c21a7a88
DJSmartyP <nickpatel.trainer@gmail.com>
2026-08-12 02:00:41 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='activate-tracker-v13-32-0';
+const CACHE='activate-tracker-v13-33-0';
 const ASSETS=[
   './','index.html','style.css','app.js','badges.json','rooms.json','manifest.webmanifest',
   'icon-192.png?v=131','icon-512.png?v=131','icon-maskable-512.png?v=131',
