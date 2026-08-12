d1a34258c1e18597c9bec57a3cfb749440f224dc
DJSmartyP <nickpatel.trainer@gmail.com>
2026-08-12 21:36:07 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const VERSION='13.75.0';
+const VERSION='13.76.0';
 const STORAGE_KEY='activateBadgeTracker_v8';
 const MAX_PINS=5;
 let BADGES=[], ROOMS=[], GAMES=[], GAME_CATALOG={}, COMPETITIVE_INFO={}, BASE_BADGE_COUNT=0;
