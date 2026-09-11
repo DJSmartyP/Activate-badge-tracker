17f60f89e739432e0763a1059587094d75b8ba61
DJSmartyP <nickpatel.trainer@gmail.com>
2026-09-11 09:42:21 +0100

    Restrict Zap and Defuse badges to Mega Laser

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE='activate-tracker-v14-14-9';
+const CACHE='activate-tracker-v14-14-10';
 const ASSETS=[
   './',
   'index.html',
   'style.css?v=1415',
   'typography-v1412.css?v=1412',
   'levels-accordion-v1412.js?v=1412',
-  'config.js?v=1410','app.js?v=1419',
+  'config.js?v=1410','app.js?v=1420',
   'badges.json','rooms.json','manifest.webmanifest',
   'icon-192.png?v=131','icon-512.png?v=131','icon-maskable-512.png?v=131','favicon-64.png?v=131',
   'icons/home.svg?v=1386','icons/badges.svg?v=1386','icons/levels.svg?v=1386','icons/competitive.svg?v=1386','icons/locations.svg?v=1386','icons/stats.svg?v=1386','icons/settings.svg?v=1386','icons/menu.svg','icons/more.svg','icons/backup.svg','icons/restore.svg',
